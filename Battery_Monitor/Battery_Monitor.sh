3d31a4b22343935d1d22f3773c31c1508f42ccba
B-Wass <101531474+B-Wass@users.noreply.github.com>
2023-07-06 16:24:10 -0400

    Update Battery_Monitor.sh

--- Battery_Monitor/Battery_Monitor.sh
+++ Battery_Monitor/Battery_Monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DISPLAY=:0 nohup python3 $(find / -name "System_Reboot.py" 2>/dev/null) >/dev/null 2>&1 &
+DISPLAY=:0 nohup python3 $(find / -name "Battery_Monitor.py" 2>/dev/null) >/dev/null 2>&1 &
